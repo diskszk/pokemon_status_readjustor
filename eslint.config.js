bf955927b82c5785c436082e0cdef91f022427c7
diskszk <ibalucky23@gmail.com>
2024-09-07 16:19:41 +0900

    feat: eslintを導入した

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,96 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import pluginJs from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
+import importPlugin from "eslint-plugin-import";
+import pluginReact from "eslint-plugin-react";
+import unusedImportsPlugin from "eslint-plugin-unused-imports";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ...pluginReact.configs.flat.recommended,
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "@stylistic": stylistic,
+      "import": importPlugin,
+      "unused-imports": unusedImportsPlugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      ...tseslint.configs.eslintRecommended.rules,
+      ...tseslint.configs.strict.rules,
+      ...tseslint.configs.strictTypeChecked.rules,
+      ...stylistic.configs["recommended-flat"].rules,
+      ...importPlugin.configs.rules,
+      "@stylistic/indent": ["error", 2],
+      "@stylistic/quotes": [
+        "error",
+        "double",
+        { allowTemplateLiterals: false },
       ],
+      "@stylistic/semi": ["error", "always"],
+      "@stylistic/arrow-parens": ["error", "always"],
+      "@stylistic/jsx-one-expression-per-line": [
+        "error",
+        { allow: "single-line" },
+      ],
+      "@stylistic/member-delimiter-style": ["error"],
+      "no-console": ["warn"],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+        },
+      ],
+      "import/no-duplicates": "error",
+      "import/order": [
+        "error",
+        {
+          "groups": [
+            "builtin",
+            "external",
+            "internal",
+            [
+              "parent",
+              "sibling",
+            ],
+            "object",
+            "type",
+            "index",
+          ],
+          "newlines-between": "always",
+          "alphabetize": {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          "pathGroups": [
+            {
+              pattern: "@/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+        },
+      ],
+      "unused-imports/no-unused-imports": "error",
     },
   },
-)
+];
