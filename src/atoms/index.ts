d2ca2d23dcd3510ea09ce12d406602f9ac21c455
diskszk <ibalucky23@gmail.com>
2024-09-13 09:18:08 +0900

    fix: atomのinitial_stateを定義

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -3,19 +3,14 @@ import { atom } from "jotai";
 import { ATK, DEF, HP, SP_ATK, SP_DEF, SPD } from "@/constants";
 import type { EffortValueState } from "@/types";
 
-export const currentEffortValueAtom = atom<EffortValueState[]>([
+export const initialState = [
   { type: HP, value: 0 },
   { type: ATK, value: 0 },
   { type: DEF, value: 0 },
   { type: SP_ATK, value: 0 },
   { type: SP_DEF, value: 0 },
   { type: SPD, value: 0 },
-]);
-export const adjustedEffortValueAtom = atom<EffortValueState[]>([
-  { type: HP, value: 0 },
-  { type: ATK, value: 0 },
-  { type: DEF, value: 0 },
-  { type: SP_ATK, value: 0 },
-  { type: SP_DEF, value: 0 },
-  { type: SPD, value: 0 },
-]);
+] satisfies EffortValueState[];
+
+export const currentEffortValueAtom = atom<EffortValueState[]>(initialState);
+export const adjustedEffortValueAtom = atom<EffortValueState[]>(initialState);
