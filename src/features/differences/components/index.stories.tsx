e16d83dc63ed777c6d9e05170441e8151689f9e8
diskszk <ibalucky23@gmail.com>
2024-10-01 15:38:40 +0900

    fix: features/differencesのstorriesを修正した

--- src/features/differences/components/index.stories.tsx
+++ src/features/differences/components/index.stories.tsx
@@ -1,34 +1,25 @@
 import React from "react";
 
-import { ATK, DEF, HP, SP_ATK, SP_DEF, SPD } from "../../../_constants";
+import { Presentation } from "./presentation";
+import { ATK, DEF, HP, SP_ATK, SP_DEF, SPD } from "../../constants";
 
 import type { Story, StoryDefault } from "@ladle/react";
 import type { ComponentProps } from "react";
 
-import { ResultTable } from ".";
-
-type Props = ComponentProps<typeof ResultTable>;
+type Props = ComponentProps<typeof Presentation>;
 
 export default {
-  title: "components/ResultTable",
+  title: "components/Differences",
   args: {
-    currentEffortValues: [
-      { type: HP, value: 252 },
-      { type: ATK, value: 252 },
+    effortValueDiff: [
+      { type: HP, value: 60 },
+      { type: ATK, value: 72 },
       { type: DEF, value: 0 },
-      { type: SP_ATK, value: 0 },
+      { type: SP_ATK, value: -84 },
       { type: SP_DEF, value: 0 },
-      { type: SPD, value: 4 },
-    ],
-    adjustedEffortValues: [
-      { type: HP, value: 192 },
-      { type: ATK, value: 180 },
-      { type: DEF, value: 0 },
-      { type: SP_ATK, value: 84 },
-      { type: SP_DEF, value: 0 },
-      { type: SPD, value: 52 },
+      { type: SPD, value: -48 },
     ],
   },
 } satisfies StoryDefault;
 
-export const Default: Story<Props> = (args) => <ResultTable {...args} />;
+export const Default: Story<Props> = (args) => <Presentation {...args} />;
