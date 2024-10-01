f817ed93082b3b3b65ee559746f74821ed2c863b
diskszk <ibalucky23@gmail.com>
2024-10-01 21:11:55 +0900

    fix: apiのURLを修正した

--- src/features/constants/config.ts
+++ src/features/constants/config.ts
@@ -1,4 +1,4 @@
-export const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT || "" as const;
+export const API_ENDPOINT = "https://beta.pokeapi.co/graphql/v1beta" as const;
 
 export const CURRENT = "current" as const;
 export const ADJUSTED = "adjusted" as const;
