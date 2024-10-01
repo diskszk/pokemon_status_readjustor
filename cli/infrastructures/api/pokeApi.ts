c0efc0d6abd684437b421ddc3fa2b0cd7ae5e468
diskszk <49810133+diskszk@users.noreply.github.com>
2024-10-01 21:13:26 +0900

    fix: apiのURLを修正した (#46)

--- cli/infrastructures/api/pokeApi.ts
+++ cli/infrastructures/api/pokeApi.ts
@@ -1,6 +1,6 @@
 import { type Result, ok, err } from "neverthrow";
 
-const apiEndpoint = process.env.VITE_API_ENDPOINT;
+const apiEndpoint = "https://beta.pokeapi.co/graphql/v1beta";
 
 export async function queryPokeAPI<T>(query: string): Promise<Result<T, Error>> {
   const response = await fetch(apiEndpoint!, {
