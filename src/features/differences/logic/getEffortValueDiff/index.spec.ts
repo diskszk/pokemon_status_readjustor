ed082581a7f3ab3602efe772ec4deaacab6d04ca
diskszk <ibalucky23@gmail.com>
2024-10-04 21:41:06 +0900

    fix: 努力値の差分の表示が+-逆になっていた箇所を修正した

--- src/features/differences/logic/getEffortValueDiff/index.spec.ts
+++ src/features/differences/logic/getEffortValueDiff/index.spec.ts
@@ -24,10 +24,37 @@ test("現在のこうげきが252, 調整後の攻撃が180の場合、差は72"
 
   expect(getEffortValueDiff(currentEffortValues, adjustedEffortValues)).toEqual([
     { value: 0, name: "hp" },
-    { value: 72, name: "attack" },
+    { value: -72, name: "attack" },
     { value: 0, name: "defense" },
     { value: 0, name: "special-attack" },
     { value: 0, name: "special-defense" },
     { value: 0, name: "speed" },
+  ]);
+});
+test("努力値がそれぞれ現在のHが4, Aが252Bが4、Dが92、Sさが156, 調整後のHが44Aが252、Bが36、Dが4のSが172の場合、差はそれぞれHが+40,Aが+-0,Bが+32,Dが-88,Sが+16である", () => {
+  const currentEffortValues = [
+    { value: 4, name: "hp" },
+    { value: 252, name: "attack" },
+    { value: 4, name: "defense" },
+    { value: 0, name: "special-attack" },
+    { value: 92, name: "special-defense" },
+    { value: 156, name: "speed" },
+  ] satisfies PokemonStatus[];
+  const adjustedEffortValues = [
+    { value: 44, name: "hp" },
+    { value: 252, name: "attack" },
+    { value: 36, name: "defense" },
+    { value: 0, name: "special-attack" },
+    { value: 4, name: "special-defense" },
+    { value: 172, name: "speed" },
+  ] satisfies PokemonStatus[];
+
+  expect(getEffortValueDiff(currentEffortValues, adjustedEffortValues)).toEqual([
+    { value: 40, name: "hp" },
+    { value: 0, name: "attack" },
+    { value: 32, name: "defense" },
+    { value: 0, name: "special-attack" },
+    { value: -88, name: "special-defense" },
+    { value: 16, name: "speed" },
   ]);
 });
--- src/features/differences/logic/getEffortValueDiff/index.ts
+++ src/features/differences/logic/getEffortValueDiff/index.ts
@@ -2,13 +2,13 @@ import type { PokemonStatus } from "@/types";
 
 export function getEffortValueDiff(currentEffortValues: PokemonStatus[],
   adjustedEffortValues: PokemonStatus[]): PokemonStatus[] {
-  return currentEffortValues.map((currentEffortValue) => {
-    const found = adjustedEffortValues.find(({ name }) => name === currentEffortValue.name);
+  return adjustedEffortValues.map((adjustedEffortValue) => {
+    const found = currentEffortValues.find(({ name }) => name === adjustedEffortValue.name);
 
     if (found) {
       return {
-        name: currentEffortValue.name,
-        value: currentEffortValue.value - found.value,
+        name: adjustedEffortValue.name,
+        value: adjustedEffortValue.value - found.value,
       };
     }
   }).filter((value) => value !== undefined);
