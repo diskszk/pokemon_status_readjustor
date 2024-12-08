6498b0e3af05a1804d31c07b5ee19455bd53a28d
diskszk <ibalucky23@gmail.com>
2024-12-08 22:33:35 +0900

    feat: 入力されたポケモン名からidを取得するqueryを追加する

---
+++ src/features/infrastructures/queries/QueryPokemonId.ts
@@ -0,0 +1,9 @@
+import { gql } from "urql";
+
+export const QueryPokemonId = gql`
+  query ($inputName: String!) {
+    pokemon_v2_pokemonspeciesname(where: {name: {_eq: $inputName}}) {
+      pokemon_species_id
+    }
+  }
+`;
