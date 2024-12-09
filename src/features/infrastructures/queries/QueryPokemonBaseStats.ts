0541f0d670d1742a091a89d0c7109040c6a148f9
diskszk <ibalucky23@gmail.com>
2024-12-10 00:10:07 +0900

    fix: PokeAPIへのqueryにidを渡すよう修正する

--- src/features/infrastructures/queries/QueryPokemonBaseStats.ts
+++ src/features/infrastructures/queries/QueryPokemonBaseStats.ts
@@ -1,8 +1,8 @@
 import { gql } from "urql";
 
 export const QueryPokemonBaseStats = gql`
-  query ($name: String!) {
-    pokemon_v2_pokemon(where: {name: {_eq: $name}}) {
+  query ($id: Int!) {
+    pokemon_v2_pokemon(where: {name: {_eq: $id}}) {
       pokemon_v2_pokemonstats {
         base_stat
         pokemon_v2_stat {
