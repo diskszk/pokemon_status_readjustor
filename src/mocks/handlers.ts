3f5fa7bc3c8e66414b8264f76c247087e0a98ecc
diskszk <ibalucky23@gmail.com>
2024-12-16 20:54:29 +0900

    feat: queryごとのhandlerを記載する

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,41 @@
 import { graphql, HttpResponse } from "msw";
 
 export const handlers = [
+  graphql.query("QueryPokemonId", () => {
+    return HttpResponse.json(
+      {
+        data: {
+          pokemon_v2_pokemonspeciesname: [
+            {
+              pokemon_species_id: 151,
+            },
+          ],
+        },
+      },
+    );
+  }),
+  graphql.query("QueryPokemonForms", () => {
+    return HttpResponse.json(
+      {
+        data: {
+          pokemon_v2_pokemonspecies: [
+            {
+              pokemon_v2_pokemons: [
+                {
+                  name: "mew",
+                  pokemon_v2_pokemonsprites: [
+                    {
+                      sprites: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      },
+    );
+  }),
   graphql.query("QueryPokemonBaseStats", () => {
     return HttpResponse.json(
       {
