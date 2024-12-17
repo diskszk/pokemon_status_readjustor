6a143efc1a67b71ec3299ca9c4923d077c6f04e5
diskszk <ibalucky23@gmail.com>
2024-12-18 00:06:30 +0900

    feat: mswをブラウザで使用する

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,24 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const mockApis = ["QueryPokemonForms", "QueryPokemonId", "QueryPokemonBaseStats"];
+
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser");
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker({ mockApis }).start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
