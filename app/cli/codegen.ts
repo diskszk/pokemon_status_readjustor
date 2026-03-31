a884855e5ebc52661255ec7d14b7a7d9ddf18643
diskszk <ibalucky23@gmail.com>
2026-03-31 21:22:41 +0900

    codegenコマンド修正

--- app/cli/codegen.ts
+++ app/cli/codegen.ts
@@ -2,10 +2,9 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "https://beta.pokeapi.co/graphql/v1beta",
-  documents: ["src/infrastructures/queries/*.query.ts"],
   ignoreNoDocuments: true,
   generates: {
-    "src/infrastructures/gql/": {
+    "infrastructures/gql/": {
       preset: "client",
       plugins: [],
     },
